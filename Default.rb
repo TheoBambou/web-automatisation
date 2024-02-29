e15ab2da9449dc55645b731148a1a7b78832b8b9
TheoBambou <104853751+TheoBambou@users.noreply.github.com>
2024-02-29 16:55:39 +0100

    Update Default.rb
    MAJ pour que la page prenne tout l'écran au lieu d'uniquement prendre le côté droit de l'écran

--- Default.rb
+++ Default.rb
@@ -1,6 +1,6 @@
 require 'watir'
 
-# Définir le chemin du pilote chromedriver
+# Chemin vers le pilote chromedriver
 Selenium::WebDriver::Chrome::Service.driver_path = '/usr/lib/chromium-browser/chromedriver'
 
 # Ouvre une nouvelle instance de Chromium avec la page 3CX
@@ -21,13 +21,15 @@ password_input.send_keys :enter
 screen_width = browser.execute_script('return screen.width')
 screen_height = browser.execute_script('return screen.height')
 
-# Calcule la nouvelle position de l'instance de Chromium et la taille pour le coté droit
-new_window_width = screen_width / 2
+# Nouvelle taille pour prendre toute la largeur et hauteur de l'écran
+new_window_width = screen_width
 new_window_height = screen_height
-new_window_x = screen_width / 2
+
+# Position en haut à gauche de l'écran
+new_window_x = 0
 new_window_y = 0
 
-# Déplace l'instance de Chromium sur la gauche et la redimensionne si besoin
+# Redimensionne et déplace la fenêtre du navigateur
 browser.window.resize_to(new_window_width, new_window_height)
 browser.window.move_to(new_window_x, new_window_y)
 sleep(5)
